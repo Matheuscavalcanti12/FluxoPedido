518117d2155311aa821d19842b77903d90d625fd
Matheuscavalcanti12 <matheuscavalcanti838@gmail.com>
2026-03-30 22:29:22 -0300

    Iniciando o projeto

--- obj/Debug/net8.0/webapi.AssemblyInfo.cs
+++ obj/Debug/net8.0/webapi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("webapi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff6b6c1904f30e3b53ab607295d6a31164e7f7dc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4fc030ca39ba2005f89aaafdc71cb265c78d762")]
 [assembly: System.Reflection.AssemblyProductAttribute("webapi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("webapi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
